d129a25ae93f80c19f19d690aebf2133b7a645e6
SlicVik <vaddamanivr1@gmail.com>
2023-04-06 13:23:18 -0400

    This is the start of the UI's for all parts of the project.

--- TestSharp/TestSharp/Program.cs
+++ TestSharp/TestSharp/Program.cs
@@ -10,12 +10,56 @@ namespace TestSharp
      {
           static void Main(string[] args)
           {
-               Console.WriteLine("Hello world!");
-               Console.WriteLine("Yo asdfasdf");
-               Console.WriteLine("Hi");
 
-                
-                Console.WriteLine("This is the test message for garrett");
+            //start of Intro Screen
+            Console.WriteLine("Project Air "); 
+            Console.WriteLine("1) Login ");
+            Console.WriteLine("2) Create Account");
+            Console.WriteLine("Enter a number to select option: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            // End of Intro Screen
+
+            // Account Creation Screen
+            if (input == 2) 
+            {
+                string usrID, password, creditcardnum, name, bday, phone, address;
+
+                Console.WriteLine("Enter into the following fields to make your account (the asterix means that the field is required)");
+
+                Console.WriteLine("User ID*: ");
+                usrID = Console.ReadLine();
+
+                Console.WriteLine("Password*: ");
+                password = Console.ReadLine();
+
+                Console.WriteLine("Credit Card Number*: ");
+                creditcardnum = Console.ReadLine();
+
+                Console.WriteLine("Name: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Birthday: ");
+                bday = Console.ReadLine();
+
+                Console.WriteLine("Phone Number: ");
+                phone = Console.ReadLine();
+
+                Console.WriteLine("Address: ");
+                address = Console.ReadLine();
+
+                if ((usrID != "") && (password != "") &&(creditcardnum != ""))
+                {
+                    Console.WriteLine("Account Made!");
+                }
+
+            }
+            else if(input == 1)
+            { 
+            }
+
+            //  Account Creation Screen
 
           }
      }
